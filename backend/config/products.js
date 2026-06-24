0fd3407f2d9c82cff6007e48b87099e8bc631fa8
francisadoche5 <francisadoche5@gmail.com>
2026-06-24 07:09:53 +0100

    Update products.js

--- backend/config/products.js
+++ backend/config/products.js
@@ -1,1 +1,20 @@
-
+module.exports = {
+  PREMIUM_1_MONTH: {
+    key: 'premium_1_month',
+    label: 'Premium 1 Month',
+    stars: 100,
+    days: 30,
+  },
+  PREMIUM_3_MONTHS: {
+    key: 'premium_3_months',
+    label: 'Premium 3 Months',
+    stars: 250,
+    days: 90,
+  },
+  PREMIUM_LIFETIME: {
+    key: 'premium_lifetime',
+    label: 'Premium Lifetime',
+    stars: 500,
+    days: null,
+  },
+};
